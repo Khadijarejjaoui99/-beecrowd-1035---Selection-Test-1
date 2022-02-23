93c558a368f1418d1804e0a2cbbe52cd35648282
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 21:58:35 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,16 @@
+// Solution of 1035 - Selection Test 1 in beecrowd
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+  int A, B, C, D;
+  cin >> A >> B >> C >> D;
+  if ((B > C && D > A) && (C + D > A + B)
+      && (C >= 0 && D >= 0) & (A % 2 == 0))
+    cout << "Valores aceitos" << endl;
+  else
+    cout << "Valores nao aceitos" << endl;
+  system("pause");
+  return 0;
+}
